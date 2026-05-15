f3f9ba585c615672f154cbe476d0f85b3b360171
Nehoray Yeframov <boris3326@gmail.com>
2026-05-15 22:22:19 +0300

    Second commit

--- backend/banksystem-personal-project-backend/src/main/java/dev/nehoray/banksystem/personal/project/backend/BankSystemApplication.java
+++ backend/banksystem-personal-project-backend/src/main/java/dev/nehoray/banksystem/personal/project/backend/BankSystemApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BankSystemApplication {
 
     public static void main(String[] args) {
-        System.out.println(System.getProperty("java.home"));
+        System.out.println(System.getProperty("java.home")); 
         System.setProperty("server.address", "0.0.0.0");
         SpringApplication.run(BankSystemApplication.class, args);
 
